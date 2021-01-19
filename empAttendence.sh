125b049beaf127596dd3cf630ba464b6a78268ce
Jaspreet <jaspreetflora7@gmail.com>
2021-01-19 14:37:56 +0530

    Calculated wages per month

--- empAttendence.sh
+++ empAttendence.sh
@@ -2,44 +2,39 @@ echo " Welcome to the Employee Wage Computation Program"
 
 isFullTime=1
 isPartTime=2
-maxHrsInMonth=4
 empRatePerHr=20
 numWorkingDays=20
+
 totalEmpHr=0
 totalWorkingDays=0
 
-randomCheck=$(($RANDOM%3))
-if [[ $randomCheck -eq $isFullTime ]]
-then
-        echo "Full Time present"
-elif [[ $randomCheck -eq $isPartTime ]]
-then
-        echo "Part Time presnt"
-else
-        echo "Absent "
-fi
-
 function getWorkHrs(){
+ while [[ $totalWorkingDays -le $numWorkingDays ]]
+ do 
+	echo "while loop"
    empCheck=$(($RANDOM%3))
    case $empCheck in 
       $isFullTime)
-	 echo "Full Time present"
- 	emphrs=8
+	 echo "Full Time present inside"
+ 	empHrs=8
 	;;
       $isPartTime)
-	 echo "Part Time present"
- 	emphrs=4
+	 echo "Part Time present inside"
+ 	empHrs=4
 	;;
    *)
-	 echo "Absent"
+	 echo "Absent inside"
 	empHrs=0
 	;;
   esac
-  
-  salary=$(($emphrs+$empRatePerHr));
-  totalSalary=$(($totalSalary+$salary));
-  echo Salary = $salary
+  ((totalWorkingDays++))
+  totalEmpHr=$(($totalEmpHr+$empHrs)) 
+done
+
+  totalSalary=$(($totalEmpHr+$empRatePerHr))
+
+  echo Total Employee Hours = $totalEmpHr
   echo Total Salary = $totalSalary
 }
+getWorkHrs
 
- getWorkHrs
