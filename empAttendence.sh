3c638e4efe7f78ed557b8ca3229c5ebb48f512ad
Jaspreet <jaspreetflora7@gmail.com>
2021-01-19 14:05:51 +0530

    Calculated Employees Daily Wage.

--- empAttendence.sh
+++ empAttendence.sh
@@ -1,6 +1,13 @@
+echo " Welcome to the Employee Wage Computation Program"
 
 isFullTime=1
 isPartTime=2
+maxHrsInMonth=4
+empRatePerHr=20
+numWorkingDays=20
+totalEmpHr=0
+totalWorkingDays=0
+
 randomCheck=$(($RANDOM%3))
 if [[ $randomCheck -eq $isFullTime ]]
 then
@@ -12,5 +19,27 @@ else
         echo "Absent "
 fi
 
+function getWorkHrs(){
+   empCheck=$(($RANDOM%3))
+   case $empCheck in 
+      $isFullTime)
+	 echo "Full Time present"
+ 	emphrs=8
+	;;
+      $isPartTime)
+	 echo "Part Time present"
+ 	emphrs=4
+	;;
+   *)
+	 echo "Absent"
+	empHrs=0
+	;;
+  esac
+  
+  salary=$(($emphrs+$empRatePerHr));
+  totalSalary=$(($totalSalary+$salary));
+  echo Salary = $salary
+  echo Total Salary = $totalSalary
+}
 
-
+ getWorkHrs
